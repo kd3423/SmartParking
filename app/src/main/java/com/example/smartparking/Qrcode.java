d29f34dc67ce7fa709273c511ee2a04374c9610a
kd3423 <karandabaskd@gmail.com>
2022-04-14 15:12:46 -0700

    QrCode class added

--- app/src/main/java/com/example/smartparking/Qrcode.java
+++ app/src/main/java/com/example/smartparking/Qrcode.java
@@ -1,4 +1,36 @@
 package com.example.smartparking;
 
+import android.graphics.Bitmap;
+
+import net.glxn.qrgen.android.QRCode;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
 public class Qrcode {
+    String userId;
+    String pId;
+    Date entryTime;
+    Bitmap qrcode;
+
+    public Qrcode(String userId, Date entryTime, String pId){
+        this.userId = userId;
+        this.entryTime = entryTime;
+        this.pId = pId;
+        generateQrCode();
+    }
+
+    private void generateQrCode(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        
+        String dateStr = dateFormat.format(this.entryTime);
+        this.qrcode = QRCode.from(this.userId+","+dateStr+","+this.pId).bitmap();
+    }
+
+    public Bitmap getQrCode(){
+        return this.qrcode;
+    }
 }
