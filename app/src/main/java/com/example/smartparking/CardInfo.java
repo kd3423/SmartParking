92457181635a4aa8dd578e8f0e31cf8976cdfe68
kd3423 <karandabaskd@gmail.com>
2022-04-14 15:27:44 -0700

    minor fix

--- app/src/main/java/com/example/smartparking/CardInfo.java
+++ app/src/main/java/com/example/smartparking/CardInfo.java
@@ -5,17 +5,30 @@ public class CardInfo {
     int securityCode;
     String nameOnCard;
     int postalCode;
-    boolean check=false;
+    boolean validateInfo=false;
 
     //  constructor
     public CardInfo(int cardNumber, int securityCode, String nameOnCard, int postalCode){
-        this.cardNumber = cardNumber;
-        this.securityCode = securityCode;
-        this.nameOnCard = nameOnCard;
-        this.postalCode = postalCode;
+        boolean c1 = false,c2 = false;
+
+        if((int)(Math.log10(cardNumber)+1) == 16){
+            c1 = true;
+        }
+
+        if((int)(Math.log10(securityCode)+1) == 3){
+            c2 = true;
+        }
+        if(c1 && c2){
+            this.cardNumber = cardNumber;
+            this.securityCode = securityCode;
+            this.nameOnCard = nameOnCard;
+            this.postalCode = postalCode;
+            this.validateInfo = true;
+        }
+
     }
 
-    public boolean authenticatePayment(CardInfo card){
-        return this.check;
+    public boolean authenticatePayment(){
+        return this.validateInfo;
     }
 }
--- app/src/main/java/com/example/smartparking/ChargePayment.java
+++ app/src/main/java/com/example/smartparking/ChargePayment.java
@@ -1,5 +1,27 @@
 package com.example.smartparking;
 
 public class ChargePayment {
+    Users u1;
+    double bill;
+    boolean status;
 
+    public ChargePayment(Users u1, double bill){
+        this.u1 = u1;
+        this.bill = bill;
+    }
+
+    public void processPayment(){
+        CardInfo c1 = u1.getCard();
+        this.status = c1.authenticatePayment();
+        if(status) {
+            u1.addBill("Bill processed successfully for $" + Double.toString(bill));
+        }
+        else{
+            u1.addBill("Unable to process for $" + Double.toString(bill)+", update card info!");
+        }
+    }
+
+    public boolean getPaymentStatus(){
+        return status;
+    }
 }
